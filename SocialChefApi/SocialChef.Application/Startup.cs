0a16701ea1aab0d2ca5d1f4f1ef8be63d1b4158d
Steve Yeager <steve.yeager@littlebytegames.com>
2020-01-03 16:10:46 +0000

    Merged PR 31: Add health check
    Related work items: #252

--- SocialChefApi/SocialChef.Application/Startup.cs
+++ SocialChefApi/SocialChef.Application/Startup.cs
@@ -1,25 +1,27 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using LittleByte.Asp.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 namespace SocialChef.Application
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
             services.AddControllers();
         }
 
+        [UsedImplicitly]
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if(env.IsDevelopment())
@@ -28,12 +30,27 @@ namespace SocialChef.Application
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
+            app.UseHealthChecks("/health", new HealthCheckOptions {ResponseWriter = WriteHealthResponse});
+
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        private static Task WriteHealthResponse(HttpContext httpContext, HealthReport report)
+        {
+            httpContext.Response.ContentType = "application/json";
+            var responseJson = JsonSerializer.Serialize(report, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Converters =
+                {
+                    new JsonStringEnumConverter(),
+                    new TimespanConverter(@"ss\:fff"),
+                }
+            });
+            return httpContext.Response.WriteAsync(responseJson);
         }
     }
 }
