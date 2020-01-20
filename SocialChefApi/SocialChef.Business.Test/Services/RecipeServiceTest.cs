1c0aaa0795d1567cf56b7cc0340ea0bdc50ccff2
Steve Yeager <steve.yeager@littlebytegames.com>
2020-01-20 12:42:11 -0800

    Create and Get recipe
    Create recipe
    
    
    Update "NUnit3TestAdapter" Version="3.16.0"
    
    
    Mark cosmos constructor for testing as UsedImplicitly
    
    
    Update dotnet core to 3.1.1
    
    
    Set PrivateAssets to compile for Persistance
    
    
    Upgrade "NSwag.AspNetCore" Version="13.2.1"
    
    
    Convert dao to dto
    
    
    Add CreateRecipeRequest
    
    
    Add GetAsync scaffolding
    
    
    Get recipe
    
    
    Correctly construct cosmos for tests and runtime

---
+++ SocialChefApi/SocialChef.Business.Test/Services/RecipeServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using LittleByte.Asp.Exceptions;
+using LittleByte.Asp.Test.Utility;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using SocialChef.Business.Requests;
+using SocialChef.Business.Services;
+using SocialChef.Persistence;
+
+namespace SocialChef.Business.Test.Services
+{
+    public class RecipeServiceTest
+    {
+        private RecipeService testObj;
+        private CosmosContext dbContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            DbContextUtility.CreateCosmosInMemory(ref dbContext, (IOptions<CosmosOptions>)null);
+
+            testObj = new RecipeService(dbContext);
+        }
+
+        [TestCase]
+        public async Task Create_Valid_CreateRecipe()
+        {
+            var request = new CreateRecipeRequest("test");
+
+            var dto = await testObj.CreateAsync(request);
+
+            Assert.AreEqual(1, dbContext.Recipes.Count());
+            Assert.IsNotNull(dto);
+        }
+
+        [Test]
+        public void Get_None_ThrowNotFound()
+        {
+            Assert.ThrowsAsync<NotFoundException>(() => testObj.GetAsync("abc"));
+        }
+
+        [Test]
+        public async Task Get_Exists_ReturnRecipe()
+        {
+            var entity = dbContext.AddAndSave(new Recipe("test"));
+
+            var found = await testObj.GetAsync(entity.ID);
+
+            Assert.AreEqual(entity.ID, found.ID);
+        }
+    }
+}
---
+++ SocialChefApi/SocialChef.Business/DTOs/RecipeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialChef.Business.DTOs
+{
+    public class RecipeDto
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        public RecipeDto(string id, string name)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
