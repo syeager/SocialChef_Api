5c18ce56ab276162c1597ee0513a26ffab7a6e3f
Steve Yeager <steve.yeager@littlebytegames.com>
2020-08-09 19:06:58 -0700

    Code cleanup

--- SocialChefApi/SocialChef.Domain.Test/ValidProperties/RecipeProps.cs
+++ SocialChefApi/SocialChef.Domain.Test/ValidProperties/RecipeProps.cs
@@ -12,15 +12,15 @@ namespace SocialChef.Domain.Test.Utilities
             public static readonly DomainGuid<Recipe>? Id = new DomainGuid<Recipe>(Guid.NewGuid());
             public static readonly DomainGuid<Chef> ChefId = new DomainGuid<Chef>(Guid.NewGuid());
             public static readonly RecipeName Name = new RecipeName(new string('a', RecipeNameValidator.LengthMin));
-            public static readonly Ingredient[] Ingredients = { IngredientProps.Ingredient };
-            public static readonly IReadOnlyList<Section> Sections = new[] { new Section(Valid.SectionName, new[] { new Step("instruction"), }) };
+            public static readonly Ingredient[] Ingredients = {IngredientProps.Ingredient};
+            public static readonly IReadOnlyList<Section> Sections = new[] {new Section(SectionName, new[] {new Step("instruction"),})};
 
             public static ModelConstructResult<Recipe> Create(Guid? id,
                 Guid? chefId = null,
-                string? name = null,
+                string name = null,
                 Guid? variantId = null,
-                IReadOnlyList<Ingredient>? ingredients = null,
-                IReadOnlyList<Section>? sections = null)
+                IReadOnlyList<Ingredient> ingredients = null,
+                IReadOnlyList<Section> sections = null)
             {
                 return Recipe.Construct(id,
                     chefId ?? ChefId,
